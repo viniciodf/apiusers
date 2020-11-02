9dea403e94c420469d6c7472c8339aed42b6cc56
viniciodf <Drhank0168>
2020-11-02 01:13:35 -0300

    fix

--- src/main/java/br/com/apiusers/ui/controllers/UsersController.java
+++ src/main/java/br/com/apiusers/ui/controllers/UsersController.java
@@ -1,6 +1,11 @@
 package br.com.apiusers.ui.controllers;
 
+import br.com.apiusers.data.UserEntity;
+import br.com.apiusers.shared.UserDto;
 import br.com.apiusers.ui.controllers.ui.model.CreateUserRequestModel;
+import br.com.apiusers.ui.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +19,9 @@ public class UsersController {
     @Autowired
     private Environment env;
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("/status/check")
     public String status(){
         return "working on port " + env.getProperty("local.server.port");
@@ -21,6 +29,13 @@ public class UsersController {
 
     @PostMapping
     public String createUser(@Valid @RequestBody CreateUserRequestModel userDetail){
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = modelMapper.map(userDetail, UserDto.class);
+
+        userService.createUser(userDto);
+
         return "create user";
     }
 }
--- src/main/java/br/com/apiusers/ui/service/UserServiceImpl.java
+++ src/main/java/br/com/apiusers/ui/service/UserServiceImpl.java
@@ -1,13 +1,31 @@
 package br.com.apiusers.ui.service;
 
+import br.com.apiusers.data.UserEntity;
+import br.com.apiusers.data.UserRepository;
 import br.com.apiusers.shared.UserDto;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDto createUser(UserDto userDetails) {
         userDetails.setUserId(UUID.randomUUID().toString());
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserEntity userEntity = modelMapper.map(userDetails, UserEntity.class);
+        userEntity.setEncryptedPassword("test");
+
+        userRepository.save(userEntity);
         return null;
     }
 }
