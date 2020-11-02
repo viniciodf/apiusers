cdf8fdba68c52e30bd07bd6f10508412079965f5
viniciodf <Drhank0168>
2020-11-01 22:42:02 -0300

    Initial commit

---
+++ src/test/java/br/com/apiusers/ApiusersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apiusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiusersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
