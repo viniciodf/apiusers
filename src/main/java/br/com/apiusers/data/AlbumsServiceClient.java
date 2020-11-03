1715ecde864cb2133f94d34871aff6d8bf063767
vinicio <vinicioferreira@sonner.com.br>
2020-11-03 01:01:27 -0300

    fix

--- src/main/java/br/com/apiusers/data/AlbumsServiceClient.java
+++ src/main/java/br/com/apiusers/data/AlbumsServiceClient.java
@@ -2,14 +2,25 @@ package br.com.apiusers.data;
 
 import br.com.apiusers.ui.controllers.ui.model.AlbumResponseModel;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@FeignClient(name="albums-ws")
+@FeignClient(name = "albums-ws", fallback = AlbumsFallback.class)
 public interface AlbumsServiceClient {
 
-    @GetMapping("/users/{id}/albumss")
+    @GetMapping("/users/{id}/albums")
     public List<AlbumResponseModel> getAlbums(@PathVariable String id);
+}
+
+@Component
+class AlbumsFallback implements AlbumsServiceClient {
+
+    @Override
+    public List<AlbumResponseModel> getAlbums(String id) {
+        return new ArrayList<>();
+    }
 }
